908a918e4709bf471c35c6de50f4624d114ced3e
SirGFM <gfmandaji@hotmail.com>
2020-05-10 15:26:58 -0300

    timer: Add method to handle a request

--- src/timer.rs
+++ src/timer.rs
@@ -1,15 +1,78 @@
 /// Remote timer and access methods to start/stop it.
 
+use std::convert::From;
+use std::convert::TryFrom;
+use std::io::Write;
+use std::net;
 use std::sync;
 use std::sync::mpsc;
 use std::time;
+use crate::tlv;
+use crate::gen_buffer;
 
+/// Commands to control the timer.
+#[derive(Clone, Copy, std::cmp::PartialEq)]
 enum Message {
     Start,
     Stop,
     Reset,
     Get,
     Quit,
+}
+
+impl std::convert::From<u8> for Message {
+    fn from(v: u8) -> Self {
+        match v {
+            0x00 => Message::Start,
+            0x01 => Message::Stop,
+            0x02 => Message::Reset,
+            0x03 => Message::Get,
+            _ => panic!("Invalid message!"),
+        }
+    }
+}
+impl std::convert::From<Message> for u8 {
+    fn from(m: Message) -> Self {
+        match m {
+            Message::Start => 0x00,
+            Message::Stop => 0x01,
+            Message::Reset => 0x02,
+            Message::Get => 0x03,
+            _ => panic!("Invalid message!"),
+        }
+    }
+}
+
+enum Status {
+    Ok,
+    NOk,
+}
+
+impl std::convert::From<u8> for Status {
+    fn from(v: u8) -> Self {
+        match v {
+            0x00 => Status::Ok,
+            0x01 => Status::NOk,
+            _ => panic!("Invalid Status"),
+        }
+    }
+}
+impl std::convert::From<Status> for u8 {
+    fn from(s: Status) -> Self {
+        match s {
+            Status::Ok => 0x00,
+            Status::NOk => 0x01,
+        }
+    }
+}
+
+/// Helper struct updated on Message::Get, and synchronized on [Timer] by
+/// a [std::sync::Mutex].
+struct SharedTimer {
+    /// Whether the struct was updated since sending the last Message::Get.
+    updated: bool,
+    /// The time elapsed on the last Message::Get.
+    cur: time::Duration,
 }
 
 /// Remote timer controlled by TCP requests.
@@ -25,10 +88,8 @@ pub struct Timer {
     /// Channel used to send messages from the main thread to the
     /// timer thread.
     tx: mpsc::Sender<Message>,
-    /// Instant when `cur` was last updated.
-    last_update: sync::Arc<sync::RwLock<time::Instant>>,
-    /// Accumulated time until since `last_update`.
-    cur: sync::Arc<sync::RwLock<time::Duration>>,
+    /// Accumulated time when `Message.Get` was last issued.
+    shared: sync::Arc<(sync::Mutex<SharedTimer>, sync::Condvar)>,
     /// Joinable handle to the timer thread. Since the scope must get
     /// ownership of the handle to call `.join()` on it, it's stored
     /// inside an `Option<T>`.
@@ -40,21 +101,21 @@ pub struct Timer {
 pub fn new_server() -> Timer {
     let (tx, rx) = mpsc::channel::<Message>();
     let now = time::Instant::now();
-    let t0 = time::Duration::from_millis(0);
-    let last_update = sync::RwLock::<time::Instant>::new(now);
-    let cur = sync::RwLock::<time::Duration>::new(t0);
 
-    let last_update = sync::Arc::new(last_update);
-    let cur = sync::Arc::new(cur);
-
-    let c_last_update = last_update.clone();
-    let c_cur = cur.clone();
+    let data = SharedTimer{
+        updated: false,
+        cur: time::Duration::from_millis(0),
+    };
+    let shared = sync::Mutex::new(data);
+    let shared = sync::Arc::new((shared, sync::Condvar::new()));
+    let c_shared = shared.clone();
 
     let thread = Some(std::thread::spawn(move || {
         let mut running = true;
         let mut did_start = false;
         let mut acc = time::Duration::from_millis(0);
         let mut now = time::Instant::now();
+        let shared = c_shared;
 
         while running {
             let recv = rx.recv();
@@ -79,36 +140,23 @@ pub fn new_server() -> Timer {
                         acc = time::Duration::from_millis(0);
                     },
                     Message::Get => {
-                        let new_cur: time::Duration;
-                        let new_now = time::Instant::now();
-
-                        if did_start {
-                            new_cur = acc + new_now.duration_since(now);
-                        } else {
-                            new_cur = time::Duration::from_millis(0);
-                        }
-
-                        {
-                            let res_w = cur.write();
-                            match res_w {
-                                Ok(mut w) => {
-                                    *w = new_cur;
-                                },
-                                Err(err) => {
-                                    println!("Failed to update the current time!");
-                                },
-                            }
-                        }
-                        {
-                            let res_w = last_update.write();
-                            match res_w {
-                                Ok(mut w) => {
-                                    *w = new_now;
-                                },
-                                Err(err) => {
-                                    println!("Failed to update the current time!");
-                                },
-                            }
+                        let (shared, cvar) = &*shared;
+                        match shared.lock() {
+                            Ok(mut data) => {
+                                if did_start {
+                                    let new_now = time::Instant::now();
+                                    let dt = new_now.duration_since(now);
+                                    data.cur = acc + dt;
+                                } else {
+                                    let t = time::Duration::from_millis(0);
+                                    data.cur = t;
+                                }
+                                data.updated = true;
+                                cvar.notify_one();
+                            },
+                            Err(err) => {
+                                println!("timer: Failed to update the current time: {}", err);
+                            },
                         }
                     },
                     Message::Quit => {
@@ -123,15 +171,103 @@ pub fn new_server() -> Timer {
 
     let t = Timer{
         tx: tx,
-        last_update: c_last_update,
-        cur: c_cur,
+        shared: shared,
         thread: thread,
     };
 
     return t;
 }
 
-impl Timer {
+type Error = &'static str;
+
+impl<'a> Timer {
+    fn send_get(&self) -> Result<time::Duration, Error> {
+        let (shared, cvar) = &*self.shared;
+        let mut data = match shared.lock() {
+            Ok(mut ok_data) => ok_data,
+            Err(err) => {
+                println!("timer: Failed to get the current time: {}", err);
+                return Err("timer: Failed to get the current time");
+            },
+        };
+
+        if let Err(err) = self.tx.send(Message::Get) {
+            println!("timer: Failed to send a get message: {}", err);
+            return Err("timer: Failed to send a get message");
+        }
+
+        while !data.updated {
+            data = match cvar.wait(data) {
+                Ok(mut ok_data) => ok_data,
+                Err(err) => {
+                    println!("timer: Failed to wait on the conditional variable the current time: {}", err);
+                    return Err("timer: Failed to wait for the current time");
+                },
+            };
+        }
+        data.updated = false;
+        return Ok(data.cur);
+    }
+
+    /// Parses and handles a given request.
+    pub fn handle_request(&self, req: &'a[u8], conn: &mut net::TcpStream) ->
+    Result<&'a[u8], Error>
+    {
+        let tp;
+        match tlv::TagParser::try_from(req) {
+            Ok(ok_tp) => tp = ok_tp,
+            Err(err) => return Err(err),
+        }
+
+        let res;
+        let m = Message::from(u8::from(&tp));
+        if m == Message::Get {
+            res = self.send_get();
+        } else {
+            res = match self.tx.send(m) {
+                Ok(_) => Ok(time::Duration::from_millis(0)),
+                Err(err) => {
+                    println!("timer: Failed to send message: {}", err);
+                    Err("timer: Failed to send message")
+                },
+            };
+        }
+
+        let mut buf;
+        match res {
+            Err(err) => {
+                buf = gen_buffer!(u8);
+                let st = u8::from(Status::NOk);
+                tlv::encode::<u8>(st, buf.as_mut_slice());
+            },
+            Ok(dur) => {
+                if m == Message::Get {
+                    buf = gen_buffer!(u8, u64, u32);
+                    let st = u8::from(Status::Ok);
+                    let next = buf.as_mut_slice();
+
+                    let next = tlv::encode::<u8>(st, next);
+                    let next = tlv::encode::<u64>(dur.as_secs(), next);
+                    tlv::encode::<u32>(dur.subsec_millis(), next);
+                } else {
+                    buf = gen_buffer!(u8);
+                    let st = u8::from(Status::Ok);
+                    tlv::encode::<u8>(st, buf.as_mut_slice());
+                }
+            },
+        }
+
+        match conn.write(buf.as_slice()) {
+            Ok(_) => {
+                return Ok(tp.get_next());
+            },
+            Err(err) => {
+                println!("timer: Failed to reply to client: {}", err);
+                return Err("Failed to reply to client");
+            },
+        }
+    }
+
     fn handle_start(&self) -> Result<(), mpsc::SendError<Message>> {
         self.tx.send(Message::Start)
     }
@@ -144,35 +280,8 @@ impl Timer {
         self.tx.send(Message::Reset)
     }
 
-    fn handle_get(&self) -> Result<(), mpsc::SendError<Message>> {
-        self.tx.send(Message::Get)
-    }
-
     fn test_get(&self) -> time::Duration {
-        let last = time::Instant::now();
-        self.handle_get().expect("Failed to get the accumulated time");
-        loop {
-            let now: time::Instant;
-            match self.last_update.read() {
-                Ok(r) => {
-                    now = *r;
-                },
-                Err(err) => {
-                    now = last;
-                },
-            }
-            if now > last {
-                break;
-            }
-        }
-        match self.cur.read() {
-            Ok(r) => {
-                *r
-            },
-            Err(err) => {
-                panic!("Failed to get the current time: {}", err);
-            }
-        }
+        self.send_get().unwrap()
     }
 }
 
@@ -196,35 +305,57 @@ impl Drop for Timer {
 mod test {
     use crate::timer;
     use std::time;
+    use std::time::Duration;
+
+    fn get_time_diff(dt: Duration, exp: Duration, err: Duration) -> Duration {
+        let diff;
+        if dt >= exp {
+            diff = dt - exp;
+        } else {
+            diff = exp - dt;
+        }
+        if diff > err {
+            println!("dt: {}us", dt.as_micros());
+            println!("exp: {}us", exp.as_micros());
+            println!("diff: {}us", diff.as_micros());
+        }
+        return diff;
+    }
 
     #[test]
     fn spawn_timer() {
         let t = timer::new_server();
 
+        // XXX: Usually, the difference stays perfectly under 0.5ms.
+        // However, I've seen it get as large as 2.5ms. This shouldn't
+        // be that big of an issue, so I just made the default threshold
+        // more lenient.
+        let err = time::Duration::from_micros(800);
+
         t.handle_start().expect("Failed to start the timer");
         let exp = time::Duration::from_millis(2);
         std::thread::sleep(exp);
         let dt = t.test_get();
-        assert!(dt > exp);
-        assert!(dt - exp < time::Duration::from_micros(500));
+        let diff = get_time_diff(dt, exp, err);
+        assert!(diff < err);
 
         t.handle_stop().expect("Failed to pause the timer");
         std::thread::sleep(time::Duration::from_millis(1));
         t.handle_start().expect("Failed to continue the timer");
         let dt = t.test_get();
-        assert!(dt > exp);
-        assert!(dt - exp < time::Duration::from_micros(500));
+        let diff = get_time_diff(dt, exp, err);
+        assert!(diff < err);
 
         t.handle_reset().expect("Failed to restart the timer");
         let exp = time::Duration::from_millis(0);
         let dt = t.test_get();
-        assert!(dt >= exp);
-        assert!(dt - exp < time::Duration::from_micros(500));
+        let diff = get_time_diff(dt, exp, err);
+        assert!(diff < err);
 
         let exp = time::Duration::from_millis(1);
         std::thread::sleep(time::Duration::from_millis(1));
         let dt = t.test_get();
-        assert!(dt > exp);
-        assert!(dt - exp < time::Duration::from_micros(500));
+        let diff = get_time_diff(dt, exp, err);
+        assert!(diff < err);
     }
 }
